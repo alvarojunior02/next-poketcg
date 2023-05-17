94813b42fae1061398ff1efbf29d25dff312a02c
Alvaro Junior <alvaroamcjunior8@gmail.com>
2023-05-17 16:25:36 -0400

    feat: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Image from "next/image"
+
+import SearchIcon from "../assets/search.png"
+import Logo from "../assets/logo.png"
+import Link from "next/link";
+
+
+export default function Footer() {
+    return (
+        <footer className="flex justify-between items-center h-40 w-full bg-zinc-300">
+            
+        </footer>
+    ) 
+}
