05a747ca3584faa119e80cc66e6a2eb37347060c
Alvaro Junior <alvaroamcjunior8@gmail.com>
2023-05-17 15:34:19 -0400

    feat: add new interfaces

---
+++ src/interfaces/set.ts
@@ -0,0 +1,16 @@
+import { ISetImage } from "./images";
+import { ILegality } from "./legality";
+
+
+export interface ISet {
+    id: string;
+    images: ISetImage;
+    legalities: ILegality;
+    name:  string;
+    printedTotal: number;
+    ptcgoCode: string;
+    releaseDate: string;
+    series:  string;
+    total: number;
+    updatedAt: string;
+}
