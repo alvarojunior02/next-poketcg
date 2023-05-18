4870d606bc3796a20b1d885f5796848005c457bf
Alvaro Junior <alvaroamcjunior8@gmail.com>
2023-05-18 10:14:30 -0400

    feat: adjust Header to page links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,26 @@
 "use client";
 import Image from "next/image"
-
-import SearchIcon from "../assets/search.png"
 import Logo from "../assets/logo.png"
-import { useState } from "react"
 import Link from "next/link";
 
 
 export default function Header() {
-    const [search, setSearch] = useState('')
     return (
-        <header className="flex justify-between items-center h-20 w-full bg-red-600 shadow-xl rounded-b-3xl">
-            <div className="ml-32">
+        <header className="flex justify-center items-center h-20 w-full bg-red-600 shadow-xl">
+            {/* <div className="ml-32">
                 <Link href={"/"}>
                     <Image src={Logo} alt="Logo" width={40} height={40}/>
                 </Link>
+            </div> */}
+            <div className="flex flex-row justify-items-center h-full">
+                <Link href={"/"} className="hover:bg-red-800 self-center h-full pt-6 pl-4 pr-4">
+                    <p className="text-white font-bold text-lg">Home</p>
+                </Link>
+                <Link href={"/sets"} className="hover:bg-red-800 self-center h-full pt-6 pl-4 pr-4">
+                    <p className="text-white font-bold text-lg">Sets</p>
+                </Link>
             </div>
-            <div className="flex flex-row justify-between items-center mr-32 h-8 w rounded bg-white">
+            {/* <div className="flex flex-row justify-between items-center mr-32 h-8 w rounded bg-white">
                 <input
                     className="h-8 w-72 pl-2 mr-2 bg-transparent outline-none"
                     value={search}
@@ -29,7 +33,7 @@ export default function Header() {
                 <button className="bg-transparent border-none pr-2">
                     <Image src={SearchIcon} alt="Search" width={20} height={20}/>
                 </button>
-            </div>
+            </div> */}
         </header>
     ) 
 }
